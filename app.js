3d2fefe728194b6a5210abc382a43cc0cacdf96a
Ajith Prakash <ajiprak@gmail.com>
2016-02-18 20:42:30 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var routerApp = angular.module('routerApp',['ui.router']);
+
+routerApp.config(function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+        .state('about',{
+
+        });
+
+});
+
+
